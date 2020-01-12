d04fd7e41b1559ea19c971f40b077c33fd55c16c
XCZTH <1490493387@qq.com>
2020-01-12 20:17:05 +0800

    first commit

---
+++ microservice-eureka02/src/main/java/com/fly/microservice/eureka02/EurekaServer02.java
@@ -0,0 +1,16 @@
+package com.fly.microservice.eureka02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by XianRui on 2020-01-12 19:49
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer02 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer02.class, args);
+    }
+}
